662fcabaa039906bb35f10b037221a72d761846e
AntonPolskij <polskij.anton707@gmail.com>
2022-01-20 00:49:20 +0300

    ДЗ №9

--- gb-react/src/components/Home/Home.js
+++ gb-react/src/components/Home/Home.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import './Home.scss';
+import { signUp, logIn } from "../../service/firebase";
 
-const Home = () => {
-    return (<>
-        <div className='home-container'>
-            <h1>Home Page</h1>
-            <Link to="chatList">Chats</Link>
-            <Link to="profile">Profile</Link>
-            <Link to="news">News</Link>
-        </div>
-    </>);
-}
-export default Home;
+export const Home = ({ isSignUp }) => {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const [error, setError] = useState("");
+    const dispatch = useDispatch();
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        try {
+            if (isSignUp) {
+                await signUp(email, pass);
+            } else {
+                await logIn(email, pass);
+            }
+            setEmail("");
+            setPass("");
+        } catch (e) {
+            setError(e.message);
+        }
+    };
+
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const onChangePass = (e) => {
+        setPass(e.target.value);
+    };
+
+    return (
+        <>
+            <h3>{isSignUp ? "Sign Up" : "Sign In"}</h3>
+            <form onSubmit={handleClick}>
+                <input type="email" value={email} onChange={onChangeEmail} />
+                <input type="password" value={pass} onChange={onChangePass} />
+                <button>SignIn</button>
+            </form>
+            {error && <h4>{error}</h4>}
+            <Link to={isSignUp ? "/" : "/signup"}>
+                {!isSignUp ? "Sign Up" : "Sign In"}
+            </Link>
+        </>
+    );
+};
